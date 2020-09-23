a9f1d219976ee2e04225a160c57b679792eab0c8
Prasenjit Purohit <github@prasenjit.net>
2020-09-23 16:45:43 +0530

    sdk interface mapping added

--- wire.go
+++ wire.go
@@ -1,6 +1,9 @@
 package core
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	"github.com/identityOrg/oidcsdk"
+)
 
 var ProviderSet = wire.NewSet(
 	NewTokenStoreServiceImpl,
@@ -9,8 +12,12 @@ var ProviderSet = wire.NewSet(
 	NewScopeClaimStoreServiceImpl,
 	NewSecretStoreServiceImpl,
 	wire.Bind(new(ITokenStoreService), new(*TokenStoreServiceImpl)),
+	wire.Bind(new(oidcsdk.ITokenStore), new(*TokenStoreServiceImpl)),
 	wire.Bind(new(ISPStoreService), new(*SPStoreServiceImpl)),
+	wire.Bind(new(oidcsdk.IClientStore), new(*SPStoreServiceImpl)),
 	wire.Bind(new(IUserStoreService), new(*UserStoreServiceImpl)),
+	wire.Bind(new(oidcsdk.IUserStore), new(*UserStoreServiceImpl)),
 	wire.Bind(new(ISecretStoreService), new(*SecretStoreServiceImpl)),
+	wire.Bind(new(oidcsdk.ISecretStore), new(*SecretStoreServiceImpl)),
 	wire.Bind(new(IScopeClaimStoreService), new(*ScopeClaimStoreServiceImpl)),
 )
